15fb629b5034c5a3d1221a71f37df9cae8b99f53
VictoriaTejera <victoriatejera15@gmail.com>
2019-06-25 12:06:32 -0300

    comienzo ls.py

--- ls.py
+++ ls.py
@@ -1,11 +1,19 @@
 #! /usr/bin/env python3
 import os
+import argparse
 
 def ls(nombres, myall = False, mydir = False, inode=False, largo = False, torden = False):
+    parser = argparse.ArgumentParser(description='banderas de la funcion')
     path = os.getcwd()
     contenido = os.scandir()
+    
+    parser.add_argument('-a', '--all',
+        help='Incluye los archivos cuyo nombre comienza con punto')
+        
+    parser.add_argument
+    
 
-    if nombres is not None:
+    if nombres:
         files = []
         dirs = []
         for nombre in nombres:
@@ -23,9 +31,10 @@ def ls(nombres, myall = False, mydir = False, inode=False, largo = False, torden
         if not myall:
            contenido = listaSinPunto(contenido)
         if torden:
-           # contenido.sort()#ordenar por fecha de modificacion
-        if inode:
             print()
+           # contenido.sort()#ordenar por fecha de modificacion
+       # if inode:
+        #    print()
 
         #contenido.sort()
 
@@ -39,6 +48,7 @@ def listaSinPunto(lista):
 
 
 def main():
+    print()
 
 
 if __name__ == '__main__':
